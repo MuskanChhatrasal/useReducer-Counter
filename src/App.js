9fd1976af21e46177563cee7cb949f3da8e9dfd3
Muskan Chhatrasal <76219453+MuskanChhatrasal@users.noreply.github.com>
2022-03-10 07:03:46 +0530

    wishlist done

--- src/App.js
+++ src/App.js
@@ -7,15 +7,26 @@ const reducer = (state, action) => {
       return { ...state, counter: state.counter + action.payload };
     case "DECREMENT":
       return { ...state, counter: state.counter - action.payload };
+    case "USER_VALUE":
+      return { ...state, user: action.payload };
+    case "ADD_USER":
+      return { ...state, wishlist: [...state.wishlist, state.user] };
     default:
       break;
   }
   return { ...state, counter: state.counter + action };
 };
-export default function App() {
-  // const [counter, setCounter] = useState(0);
 
-  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+export default function App() {
+  // const [value, setValue] = useState([]);
+  // const [user, setUser] = useState("");
+
+  const [state, dispatch] = useReducer(reducer, {
+    counter: 0,
+    user: "",
+    wishlist: []
+  });
+
   return (
     <div className="App">
       <h1>{state.counter}</h1>
@@ -25,6 +36,21 @@ export default function App() {
       <button onClick={() => dispatch({ type: "DECREMENT", payload: 10 })}>
         -
       </button>
+
+      <h2>Wishlist</h2>
+      <label>
+        Your Wish:
+        <input
+          type="text"
+          onChange={(e) =>
+            dispatch({ type: "USER_VALUE", payload: e.target.value })
+          }
+        ></input>
+      </label>
+      <button onClick={() => dispatch({ type: "ADD_USER" })}>Add</button>
+      {state.wishlist.map((val) => {
+        return <p key={val}>{val}</p>;
+      })}
     </div>
   );
 }
